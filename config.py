74c652ff652d826d76f3551564d239d746a8e0b1
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2023-12-13 03:43:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Twilight-Killer/MusicBot",
+    "https://github.com/Twilight-Killer/Kimochi-MusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
