8cc57eb4b9ec449983040b77111b6f6a77f7f7ad
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2023-12-12 22:17:26 +0700

    Update autoleave.py

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001897476603
-                                and i.chat.id != -1001704645461
+                                and i.chat.id != -1001787435726
+                                and i.chat.id != -1001526106346
                             ):
                                 if left == 20:
                                     continue
